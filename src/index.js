dd732045cd15cb38f65662422db35351adbeb857
n9peje00 <n9peje00@students.oamk.fi>
2023-02-20 19:31:07 +0200

    Full project

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// import { initializeApp } from "import { } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js'";
+// import { getDatabase } from "import { } from 'https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js'";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, set } from "firebase/database";
+import { firebaseConfig } from "./config.mjs";
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+// const ref = ref(db, '/selection/');
+
+const submit = document.getElementById('submit_form');
+
+function getVoterID(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+submit.addEventListener("click", writeData);
+
+function writeData() {
+  const db = getDatabase();
+  const voterID = getVoterID(8);
+  let answer = document.getElementById("answer").value;
+  console.log(answer);
+  set(ref(db, 'votes/' + voterID + '/'), {
+    voter: voterID,
+    selectedPupper: answer
+  });
+}
+
